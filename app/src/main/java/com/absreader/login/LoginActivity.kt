f80e56b3c0f3899ca5afce2e35223fb370c49222
jordan95v <90615280+jordan95v@users.noreply.github.com>
2024-11-28 09:13:26 +0100

    Added login http request.

--- app/src/main/java/com/absreader/login/LoginActivity.kt
+++ app/src/main/java/com/absreader/login/LoginActivity.kt
@@ -4,10 +4,16 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.absreader.R
 import com.absreader.login.models.LoginParameters
 import com.absreader.networks.RetrofitFactory
+import com.absreader.networks.dto.LoginDTO
+import com.absreader.networks.services.AuthenticationService
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 import retrofit2.Retrofit
 
 class LoginActivity : AppCompatActivity() {
@@ -15,15 +21,45 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_login)
-
-        val server: String = findViewById<EditText>(R.id.server).toString()
-        val username: String = findViewById<EditText>(R.id.username).toString()
-        val password: String = findViewById<EditText>(R.id.password).toString()
+        val server: EditText = findViewById<EditText>(R.id.server)
+        val username: EditText = findViewById<EditText>(R.id.username)
+        val password: EditText = findViewById<EditText>(R.id.password)
         val login: Button = findViewById<Button>(R.id.loginButton)
-
         login.setOnClickListener {
-            val client: Retrofit = RetrofitFactory.get_instance(server)
-            val loginParameters: LoginParameters = LoginParameters(username, password)
+            val client: Retrofit = RetrofitFactory.get_instance(server.text.toString())
+            val loginParameters: LoginParameters = LoginParameters(
+                username.text.toString(), password.text.toString()
+            )
+            val call: Call<LoginDTO> = client.create(AuthenticationService::class.java).login(
+                loginParameters
+            )
+            call.enqueue(object : Callback<LoginDTO> {
+                override fun onResponse(call: Call<LoginDTO>, response: Response<LoginDTO>) {
+                    if (response.isSuccessful) {
+                        val loginDTO: LoginDTO? = response.body()
+                        with(getSharedPreferences("com.absreader", MODE_PRIVATE).edit()) {
+                            putString("bearer", loginDTO?.user?.token)
+                            apply()
+                        }
+                    } else {
+                        val dialog: AlertDialog = AlertDialog.Builder(this@LoginActivity)
+                            .setTitle("Error")
+                            .setMessage("Invalid credentials, please try again")
+                            .setPositiveButton("OK", null)
+                            .create()
+                        dialog.show()
+                    }
+                }
+
+                override fun onFailure(call: Call<LoginDTO>, t: Throwable) {
+                    val dialog: AlertDialog = AlertDialog.Builder(this@LoginActivity)
+                        .setTitle("Error")
+                        .setMessage("An error occurred, please try again, check server address")
+                        .setPositiveButton("OK", null)
+                        .create()
+                    dialog.show()
+                }
+            })
         }
     }
 }
---
+++ app/src/main/java/com/absreader/networks/services/AuthenticationService.kt
@@ -0,0 +1,12 @@
+package com.absreader.networks.services
+
+import com.absreader.login.models.LoginParameters
+import com.absreader.networks.dto.LoginDTO
+import retrofit2.Call
+import retrofit2.http.Body
+import retrofit2.http.POST
+
+interface AuthenticationService {
+    @POST("login")
+    fun login(@Body loginParameters: LoginParameters): Call<LoginDTO>
+}
