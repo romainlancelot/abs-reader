a7adf1084f85e8b5aeba297f9509b1455be99244
Thomas HUYNH <thomashuynh795@gmail.com>
2025-01-20 13:45:11 +0100

    Imports corrected and source code linted.

--- api/src/auth/auth.service.ts
+++ api/src/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, NotFoundException, UnauthorizedException } from "@nestjs/common";
+import { Injectable } from "@nestjs/common";
 import { PrismaService } from "src/prisma/prisma.service";
 import { hash, compare } from "bcrypt";
 import { JwtService } from "@nestjs/jwt";
